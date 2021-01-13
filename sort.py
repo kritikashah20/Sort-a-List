59e4c991f6496b12f900687746ca8c20b90be47c
Kritika Shah <kritishah2017@gmail.com>
2021-01-13 21:18:51 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,15 @@
+# Create a List
+cars = ['Ford', 'BMW', 'Volvo']
+
+# sort the list
+cars.sort()
+
+# print the sorted list
+print(cars)
+
+# Sort the list in reverse order
+cars = ['Ford', 'BMW', 'Volvo']
+
+cars.sort(reverse=True)
+
+print(cars)
